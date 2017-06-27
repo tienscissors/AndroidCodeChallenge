15fb82e823c20e795dd30a3f29d91d80c78ee8de
Tien Le <tienl@hostworks.com.au>
2017-06-27 09:46:39 +0930

    - Cleaned up code - Added comments (more than usual) - Added more automatic tests

--- app/src/main/java/com/example/androidcodechallenge/MainActivity.java
+++ app/src/main/java/com/example/androidcodechallenge/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.androidcodechallenge;
 
 import android.graphics.Color;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.DisplayMetrics;
-import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 
 import com.example.androidcodechallenge.view.Timer;
 
@@ -24,45 +22,9 @@ public class MainActivity extends AppCompatActivity {
 
         timer = (Timer) findViewById(R.id.timer);
 
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-
-        final int width = displayMetrics.widthPixels;
-
-        ViewTreeObserver observer = timer.getViewTreeObserver();
-        observer.addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
-            @Override
-            public boolean onPreDraw() {
-                ViewTreeObserver observer = timer.getViewTreeObserver();
-                if(observer != null) {
-                    observer.removeOnPreDrawListener(this);
-                }
-
-                timer.setSize(width / 2);
-
-                timer.setRingThickness(40);
-
-                return false;
-            }
-        });
-
-
-        //TESTS
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                timer.setRingThickness(20);
-            }
-        }, 5000);
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                timer.setThemeColor(Color.RED);
-            }
-        }, 8000);
+        //TEST CONFIGURATIONS
+        runAutomatedTests();
     }
-
 
     @Override
     protected void onResume() {
@@ -84,4 +46,54 @@ public class MainActivity extends AppCompatActivity {
 
         timer.stop();
     }
+
+    private void runAutomatedTests() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+
+        final int width = displayMetrics.widthPixels;
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                timer.setRingThickness(20);
+            }
+        }, 5000);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                timer.setThemeColor(Color.RED);
+            }
+        }, 8000);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                timer.setSize(width / 2);
+                timer.setRingThickness(60);
+            }
+        }, 12000);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                timer.setSize(width * 2 / 3);
+                timer.setRingThickness(80);
+                timer.setThemeColor(Color.YELLOW);
+            }
+        }, 18000);
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                timer.setSize(width);
+                timer.setRingThickness(40);
+                timer.setThemeColor(Timer.DEFAULT_COLOR);
+            }
+        }, 22000);
+
+    }
+
 }
--- app/src/main/java/com/example/androidcodechallenge/view/Timer.java
+++ app/src/main/java/com/example/androidcodechallenge/view/Timer.java
@@ -8,9 +8,9 @@ import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.SweepGradient;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.LinearInterpolator;
@@ -28,35 +28,43 @@ public class Timer extends View {
     private static final String TAG = Timer.class.getSimpleName();
 
 
+    //Using SimpleDateFormat is probably easiest way for use to convert time into required format
     private static final SimpleDateFormat TIMER_FORMAT = new SimpleDateFormat("mm:ss");
 
     static {
+        //Need to set to GMT else it will add timezone offset to formats
         TIMER_FORMAT.setTimeZone(TimeZone.getTimeZone("GMT"));
     }
 
+    //Default values
     public static final int DEFAULT_THICKNESS = 50; //pixels
 
     public static final int DEFAULT_COLOR = 0xff458dd2; //the light blue from the example
 
-    public static final int DEFAULT_TIME = 20000; //20 seconds
-
+    public static final int DEFAULT_TIME = 2 * 60 * 1000; //2 minutes
 
     public static final int SECONDARY_TEXT_COLOR = 0xffababab; //text color for 2 bottom strings
 
 
+    //Static / hard coded text, should change dynamically in real situation
     private static final String MOVES = "8 MOVES";
 
     private static final String REMAINING = "REMAINING";
 
 
-    //Modified code snippet here: https://stackoverflow.com/questions/12166476/android-canvas-drawtext-set-font-size-from-width
-    private static TextAttributes setTextAttributes(TextAttributes attr, Paint paint, float desiredWidth,
+    /**
+     * Modified code snippet here: https://stackoverflow.com/questions/12166476/android-canvas-drawtext-set-font-size-from-width
+     *
+     * Calculates the necessary text size for the text to fit the desiredWidth
+     *
+     * @param attr TextAttributes object to store calculations
+     * @param paint The paint used to draw/calculate
+     * @param desiredWidth The width we want to fit the text into
+     * @param text The text to be drawn
+     *
+     */
+    private static void setTextAttributes(@NonNull TextAttributes attr, Paint paint, float desiredWidth,
                                                     String text) {
-
-        if(attr == null) {
-            attr = new TextAttributes();
-        }
-
         // Pick a reasonably large value for the test. Larger values produce
         // more accurate results, but may cause problems with hardware
         // acceleration. But there are workarounds for that, too; refer to
@@ -77,32 +85,46 @@ public class Timer extends View {
         paint.getTextBounds(text, 0, text.length(), bounds);
 
         attr.textHeight = bounds.height();
-
-        return attr;
     }
 
 
-
-
+    //For drawing the Ring
     private Paint arcPaint = new Paint();
 
     private RectF arcRect = new RectF(0, 0, 0, 0);
 
+    //For drawing the text
     private Paint textPaint = new Paint();
 
+    private TextAttributes timeTextAttr = new TextAttributes();
+
+    private TextAttributes movesTextAttr = new TextAttributes();
+
+    private TextAttributes remainingTextAttr = new TextAttributes();
+
+    private Date timeDate = new Date(DEFAULT_TIME);
+
+    private String timeString = TIMER_FORMAT.format(timeDate);
+
+    //vertical spacing between text lines, we make this dynamic too
+    private int textSpacing;
+
+    private int initialTextYTranslation;
+
+
+    //For Animations
     private ObjectAnimator ringRotationAnimator;
 
     private ObjectAnimator timeAnimator;
 
     private int rotation = -90;
 
+    //Theme color
     private int color = DEFAULT_COLOR;
 
-    private Date timeDate = new Date(DEFAULT_TIME);
+    //So we don't have to keep calculating half size
+    private int halfSize;
 
-    private String timeString = TIMER_FORMAT.format(timeDate);
-
-    private int textWidth;
 
 
     public Timer(Context context, @Nullable AttributeSet attrs) {
@@ -111,7 +133,7 @@ public class Timer extends View {
         arcPaint.setStyle(Paint.Style.STROKE);
         arcPaint.setStrokeWidth(DEFAULT_THICKNESS);
 
-        textPaint.setTextSize(60);
+        //Align text paint to be horizontally centered so we don't have to translate x
         textPaint.setTextAlign(Paint.Align.CENTER);
         //Should set typeface here
 //        textPaint.setTypeface()
@@ -134,10 +156,16 @@ public class Timer extends View {
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
 
+        halfSize = w / 2;
+
+        //We have to recalibrate our paint after every size change
         setRingThickness((int) arcPaint.getStrokeWidth());
         setThemeColor(color);
     }
 
+    /**
+     * Starts the animations for ring rotation and timer
+     */
     public void start() {
         ringRotationAnimator = ObjectAnimator.ofInt(this, "ringRotation", -90, 270).setDuration(1000);
         ringRotationAnimator.setRepeatCount(-1);
@@ -150,15 +178,22 @@ public class Timer extends View {
         timeAnimator.start();
     }
 
+    /**
+     * Stops / Cancels the animation
+     */
     public void stop() {
         ringRotationAnimator.cancel();
-
-        rotation = -90;
-        invalidate();
+        timeAnimator.cancel();
     }
 
+    /**
+     * Pauses the animation
+     *
+     * Doesn't work, needs a resume, we won't do for this sample
+     */
     public void pause() {
         ringRotationAnimator.pause();
+        timeAnimator.pause();
     }
 
     public void setRingRotation(int rotation) {
@@ -167,10 +202,12 @@ public class Timer extends View {
     }
 
     public void setTime(int time) {
+        //If time has finished, stop the animation
         if(time <= 0) {
             stop();
         }
 
+        //Set the time string using value from animator
         timeDate.setTime(time);
 
         timeString = TIMER_FORMAT.format(timeDate);
@@ -199,22 +236,23 @@ public class Timer extends View {
         arcRect.left = arcRect.top = thickness / 2;
         arcRect.right = arcRect.bottom = getMeasuredWidth() - (thickness / 2);
 
-        textWidth = (int) ((getMeasuredWidth() - (2 * thickness)) * 0.6);
+        //Hard coding the allocated space for text to be 60% of available space within the ring
+        int textWidth = (int) ((getMeasuredWidth() - (2 * thickness)) * 0.6);
 
+        //Calculate the size and height of the text to fit in our allocated space
         setTextAttributes(timeTextAttr, textPaint, textWidth, timeString);
 
         setTextAttributes(movesTextAttr, textPaint, textWidth, MOVES);
 
         setTextAttributes(remainingTextAttr, textPaint, textWidth, REMAINING);
+
+        //Hard coding the space to be 20% of timer text height
+        textSpacing = (int) (timeTextAttr.textHeight * 0.2);
+
+        //Text is aligned to the bottom, so we negative translate the total_height / 2, and then positive translate the timer text height
+        //We calculate it here so we don't have to calculate every draw loop
+        initialTextYTranslation = (int) (timeTextAttr.textHeight-((timeTextAttr.textHeight + movesTextAttr.textHeight + remainingTextAttr.textHeight + (textSpacing * 2)) / 2));
     }
-
-    private TextAttributes timeTextAttr = new TextAttributes();
-
-    private TextAttributes movesTextAttr = new TextAttributes();
-
-    private TextAttributes remainingTextAttr = new TextAttributes();
-
-
 
     /**
      * Sets the color for the ring and the timer text
@@ -232,30 +270,43 @@ public class Timer extends View {
 
         canvas.save();
 
-        canvas.rotate(rotation, canvas.getWidth() / 2, canvas.getHeight() / 2);
+        //We rotate according to the animation
+        canvas.rotate(rotation, halfSize, halfSize);
+
+        //Draw the arc / ring
         canvas.drawArc(arcRect, 0, 360, false, arcPaint);
 
         canvas.restore();
 
+
+
         canvas.save();
-        canvas.translate(0, -((timeTextAttr.textHeight + movesTextAttr.textHeight + remainingTextAttr.textHeight) / 2));
+
+        canvas.translate(0, initialTextYTranslation);
 
         //Draw timer
         textPaint.setColor(color);
         textPaint.setTextSize(timeTextAttr.textSize);
-        canvas.drawText(timeString, canvas.getWidth() / 2, canvas.getHeight() / 2, textPaint);
+        canvas.drawText(timeString, halfSize, halfSize, textPaint);
 
         textPaint.setColor(SECONDARY_TEXT_COLOR);
 
+        //Draw Moves Text
         textPaint.setTextSize(movesTextAttr.textSize);
-        canvas.translate(0, timeTextAttr.textHeight);
-        canvas.drawText(MOVES, canvas.getWidth() / 2, canvas.getHeight() / 2, textPaint);
+        canvas.translate(0, movesTextAttr.textHeight + textSpacing);
+        canvas.drawText(MOVES, halfSize, halfSize, textPaint);
 
+        //Draw Remaining text
         textPaint.setTextSize(remainingTextAttr.textSize);
-        canvas.translate(0, movesTextAttr.textHeight);
-        canvas.drawText(REMAINING, canvas.getWidth() / 2, canvas.getHeight() / 2, textPaint);
+        canvas.translate(0, remainingTextAttr.textHeight + textSpacing);
+        canvas.drawText(REMAINING, halfSize, halfSize, textPaint);
+
+        canvas.restore();
     }
 
+    /**
+     * Just a simple object to store our text size / height attributes
+     */
     static class TextAttributes {
         float textSize;
         float textHeight;
